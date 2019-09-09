ab1c046641f2b9f4773d0cb0d6e3e307910f398f
Luis Godinez <luisgodinez@Luiss-MacBook-Air.local>
2019-09-08 23:08:14 -0600

    base start

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,112 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
-import Nav from '../components/nav'
+// import React from 'react'
+// import Link from 'next/link'
+// import Head from 'next/head'
+// import Nav from '../components/nav'
+// //import 'bootstrap/dist/css/bootstrap.css';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
+// const Home = () => (
+//   <div>
 
-    <Nav />
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+//     <Head>
+//       <title>Home</title>
+//     </Head>
 
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#setup'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
-      </div>
-    </div>
+    
+//     <div className='hero'>
+//       <h1 className='title'>Welcome to Next.js!</h1>
+//       <p className='description'>
+//         To get started, edit <code>pages/index.js</code> and save to reload.
+//       </p>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+//       <div className='row'>
+//         <Link href='map'>
+//           <a className='card'>
+//             <h3>Getting Started &rarr;</h3>
+//             <p>Learn more about Next.js on GitHub and in their examples.</p>
+//           </a>
+//         </Link>
+//       </div>
+//       <div className='row'>
+//         <Link href='Profile'>
+//           <a className='card'>
+//             <h3>Getting Started &rarr;</h3>
+//             <p>Learn more about Next.js on GitHub and in their examples.</p>
+//           </a>
+//         </Link>
+//       </div>
+//     </div>
 
-export default Home
+//     <style jsx>{`
+//       .hero {
+//         width: 100%;
+//         color: #333;
+//       }
+//       .title {
+//         margin: 0;
+//         width: 100%;
+//         padding-top: 80px;
+//         line-height: 1.15;
+//         font-size: 48px;
+//       }
+//       .title,
+//       .description {
+//         text-align: center;
+//       }
+//       .row {
+//         max-width: 880px;
+//         margin: 80px auto 40px;
+//         display: flex;
+//         flex-direction: row;
+//         justify-content: space-around;
+//       }
+//       .card {
+//         padding: 18px 18px 24px;
+//         width: 220px;
+//         text-align: left;
+//         text-decoration: none;
+//         color: #434343;
+//         border: 1px solid #9b9b9b;
+//       }
+//       .card:hover {
+//         border-color: #067df7;
+//       }
+//       .card h3 {
+//         margin: 0;
+//         color: #067df7;
+//         font-size: 18px;
+//       }
+//       .card p {
+//         margin: 0;
+//         padding: 12px 0 0;
+//         font-size: 13px;
+//         color: #333;
+//       }
+//     `}</style>
+//   </div>
+// )
+
+// export default Home
+
+
+import Container from "react-bootstrap/Container";
+import fetch from "isomorphic-fetch";
+import Thoughts from "../components/Thoughts";
+
+function Index(props) {
+  return (
+    <Container>
+      <Thoughts thoughts={props.thoughts} />
+    </Container>
+  );
+}
+
+Index.getInitialProps = async ({ req }) => {
+  const baseURL = req ? `${req.protocol}://${req.get("Host")}` : "";
+  const res = await fetch(`${baseURL}/api/thoughts`);
+  return {
+    thoughts: await res.json()
+  };
+};
+
+export default Index;
